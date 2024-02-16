28aa9e3a28a35e2663b90fddc31a06ead9c2c709
ariyabualuang <bualuangariya@gmail.com>
2024-02-16 09:37:54 +0700

    change time

--- line.js
+++ line.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 require('dotenv').config();
 
-const channelAccessToken  = "r11Xod8be7hTlVyVzNh9722k4y6NMnFQBkwDpOI5+MG0we4OJYffgfZK4a1fE9d1WjvXQte2WocnO22cneUIxAwM6qI2jRoa2yBISnlKXfcdDyQVdeB6YY3XF2xC5vElclKmshMaq7EovZeM8AbpgwdB04t89/1O/w1cDnyilFU=";
+const channelAccessToken  = "mhfoz6dR56oza7pbTl8rLIGkDlFzDSxeJ+wlmMQvLl1mfBgUfmokS0x+J2LUebj+erc6uNXAZ0TR0cEUORlnsoYPjxum4lUrmXShI0PNaZx5bNDsAQ8M5KkpPxytkJ/MkJ0K5cupsledAzvyqDIgRAdB04t89/1O/w1cDnyilFU=";
 
 module.exports = {
     sendLineMsgBroadcast: async (messages, token = channelAccessToken) => {
