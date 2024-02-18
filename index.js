1712aba984cd968b2ab97b448f69e609520fb482
ariyabualuang <bualuangariya@gmail.com>
2024-02-19 00:17:17 +0700

    TT

--- index.js
+++ index.js
@@ -12,9 +12,7 @@ var todo= "";
 var todo2 = "";
 var color = "";
 //scheduleJob('52 10 * * *', async () => {
-app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
-  });
+
 app.get('/', asyncHandler(async (req, res, next)=> {
       
     try {
@@ -129,6 +127,9 @@ app.get('/', asyncHandler(async (req, res, next)=> {
             }]
         );
         health="";
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
         
           
           
